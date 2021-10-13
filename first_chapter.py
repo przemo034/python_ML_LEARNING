c34197a895887aa63a0a8e840dcdb938c1f6bfa2
Przemek <przemo034jado@gmail.com>
2021-10-13 20:43:13 +0200

    my first repository

---
+++ first_chapter.py
@@ -0,0 +1,28 @@
+import sys
+import pandas as pd
+import matplotlib
+import numpy as np
+import scipy as sp
+import IPython
+import sklearn
+
+import sys
+print("Python version:", sys.version)
+
+import pandas as pd
+print("pandas version:", pd.__version__)
+
+import matplotlib
+print("matplotlib version:", matplotlib.__version__)
+
+import numpy as np
+print("NumPy version:", np.__version__)
+
+import scipy as sp
+print("SciPy version:", sp.__version__)
+
+import IPython
+print("IPython version:", IPython.__version__)
+
+import sklearn
+print("scikit-learn version:", sklearn.__version__)
