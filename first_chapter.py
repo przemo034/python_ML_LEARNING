9955ee0a6a14264cc71921efb565153c3b96e194
Przemek <przemo034jado@gmail.com>
2021-10-13 20:54:01 +0200

    change file

--- first_chapter.py
+++ first_chapter.py
@@ -6,23 +6,3 @@ import scipy as sp
 import IPython
 import sklearn
 
-import sys
-print("Python version:", sys.version)
-
-import pandas as pd
-print("pandas version:", pd.__version__)
-
-import matplotlib
-print("matplotlib version:", matplotlib.__version__)
-
-import numpy as np
-print("NumPy version:", np.__version__)
-
-import scipy as sp
-print("SciPy version:", sp.__version__)
-
-import IPython
-print("IPython version:", IPython.__version__)
-
-import sklearn
-print("scikit-learn version:", sklearn.__version__)
